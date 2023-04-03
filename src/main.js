ac7655de93484c828883ee0247170dd986c048df
Kichan Yoon <pro4697@naver.com>
2023-04-03 18:46:22 +0900

    feat: three.js template 추가

--- src/main.js
+++ src/main.js
@@ -1,7 +1,83 @@
-// import * as THREE from 'three';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-window.addEventListener('load', function () {
-	init();
+window.addEventListener('load', () => {
+  init();
 });
 
-function init() {}
+function init() {
+  const renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialias: true,
+  });
+
+  const container = document.querySelector('#app');
+
+  container.appendChild(renderer.domElement);
+
+  const canvasSize = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+
+  // canvas 사이즈 지정
+  renderer.setSize(canvasSize.width, canvasSize.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    canvasSize.width / canvasSize.height,
+    0.1,
+    100,
+  );
+  camera.position.set(0, 0, 3);
+
+  // 마우스 드래그 컨트롤
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  // undrag시 부드럽게 멈추기 (damping)
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.1;
+
+  // 테스트 object
+  const createObject = () => {
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const geometry = new THREE.PlaneGeometry(1, 1);
+    const mesh = new THREE.Mesh(geometry, material);
+
+    scene.add(mesh);
+  };
+
+  const resize = () => {
+    canvasSize.width = window.innerWidth;
+    canvasSize.height = window.innerHeight;
+
+    camera.aspect = canvasSize.width / canvasSize.height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(canvasSize.width, canvasSize.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  };
+
+  const addEvent = () => {
+    window.addEventListener('resize', resize);
+  };
+
+  const draw = () => {
+    controls.update();
+    renderer.render(scene, camera);
+    requestAnimationFrame(() => {
+      draw();
+    });
+  };
+
+  const initialize = () => {
+    createObject();
+    addEvent();
+    resize();
+    draw();
+  };
+
+  initialize();
+}
