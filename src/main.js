c638742c8eced106a038523a38ee41ac6616b80d
Kichan Yoon <pro4697@naver.com>
2023-04-03 15:05:07 +0900

    feat: boilerplate 생성

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// import * as THREE from 'three';
+
+window.addEventListener('load', function () {
+	init();
+});
+
+function init() {}
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+/** @see {import('vite').userConfig} */
+export default {
+	root: 'src',
+	build: {
+		ourDir: '../dist',
+		emptyOutDir: true,
+	},
+	server: { port: 3000 },
+};
