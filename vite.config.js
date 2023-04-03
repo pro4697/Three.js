c38450848510c116f6b0684b1eb1b44c02f9c884
Kichan Yoon <pro4697@naver.com>
2023-04-04 01:13:24 +0900

    chore: index.html 위치 변경

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 /** @see {import('vite').userConfig} */
 export default {
-	root: 'src',
-	build: {
-		ourDir: '../dist',
-		emptyOutDir: true,
-	},
-	server: { port: 3000 },
+  build: {
+    ourDir: '../dist',
+    emptyOutDir: true,
+  },
+  server: { port: 3000 },
 };
